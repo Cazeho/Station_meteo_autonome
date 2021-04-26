f5e280949966f9673726bdfd654a0c7902f55c16
Cazeho <58745332+Cazeho@users.noreply.github.com>
2021-04-27 00:49:37 +0200

    Create access_point.ino

---
+++ access_point.ino
@@ -0,0 +1,28 @@
+/////////////////////////////////
+// Generated with a lot of love//
+// with TUNIOT FOR ESP32     //
+// Website: Easycoding.tn      //
+/////////////////////////////////
+#include <WiFi.h>
+
+void setup()
+{
+Serial.begin(9600);
+
+  delay(3000);
+  Serial.println("START");
+  WiFi.softAP("ssid","passwd");
+  Serial.println("ip of AP");
+  Serial.println((WiFi.softAPIP()));
+
+}
+
+
+void loop()
+{
+
+    Serial.println("Connected");
+    Serial.println((WiFi.softAPgetStationNum()));
+    delay(1000);
+
+}
