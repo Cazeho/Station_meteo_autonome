0ff91f051f8d29490eef79942f85be962aeda39b
Cazeho <58745332+Cazeho@users.noreply.github.com>
2021-05-27 09:10:42 +0200

    Update receive.ino

--- receive.ino
+++ receive.ino
@@ -23,3 +23,32 @@ void loop() {
     Serial.println(donnee, DEC);
   }
 }
+
+//
+#include <SoftwareSerial.h>
+ 
+SoftwareSerial mySerial(16, 17);
+
+
+int donnee = 0; // for incoming serial data
+
+void setup() {
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  mySerial.begin(1200);
+  Serial.println("Hi, I'm going to receive message!");
+ 
+}
+
+void loop() {
+  // send data only when you receive data:
+  if (mySerial.available()) {
+    // read the incoming byte:
+    donnee = mySerial.read();
+
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println((char)donnee);
+    delay(500);
+  }
+}
+
